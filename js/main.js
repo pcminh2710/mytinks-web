b818ceefdc9a54a3a29e63852ff315378bc2d778
= <=>
2021-07-27 12:36:53 +0700

    create project

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    
+    var checkBox = document.getElementById("myCheck");
+    
+    if (checkBox.checked == true){
+      alert("Bạn đã chấp nhận điều khoản");
+    } else {
+      alert("Bạn chưa chấp nhận điều khoản");
+    }
+  }
